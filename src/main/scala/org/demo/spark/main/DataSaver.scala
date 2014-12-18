91cabe5905380aeba2953ea0785daaf42fe4ee3e
BogdanCojocar <bogdan.cojocar@gmail.com>
2014-12-18 22:56:20 +0200

    updated DataSaver

--- src/main/scala/org/demo/spark/main/DataSaver.scala
+++ src/main/scala/org/demo/spark/main/DataSaver.scala
@@ -6,6 +6,8 @@ import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.hadoop.conf.Configuration
 import org.bson.BasicBSONObject
+import com.mongodb.casbah.commons.MongoDBList
+import com.mongodb.casbah.commons.MongoDBObject
 
 object DataSaver {
   val mongoUri = "mongodb://127.0.0.1:27017/gpxdata"
@@ -19,13 +21,17 @@ class DataSaver {
   val config = new Configuration()
   config.set("mongo.output.uri", DataSaver.mongoUri)
 
-  def save(gpxData: RDD[Any]) = {
+  def save(gpxData: RDD[List[GPXData]]) = {
 
     val sparkConf = new SparkConf()
-      
+
     val geoJsonData = gpxData.map(data => {
-      var bson = new BasicBSONObject()
-      (null, bson)
+      val geoJson = MongoDBObject(
+        "loc" -> MongoDBObject(
+          "type" -> "MultiPoint",
+          "coordinates" -> MongoDBList(data)))
+
+      (null, geoJson)
     })
 
     geoJsonData.saveAsNewAPIHadoopFile(DataSaver.hadoopDummyFile, classOf[Any], classOf[Any], classOf[com.mongodb.hadoop.MongoOutputFormat[Any, Any]], config)
