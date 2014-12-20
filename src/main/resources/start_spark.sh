83db85b13d5b2708f45cdd6bdb29720ccd9c0c5e
BogdanCojocar <bogdan.cojocar@gmail.com>
2014-12-20 15:10:37 +0200

    removed resources folder

--- src/main/resources/start_spark.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd Development/spark-1.1.0-bin-hadoop2.4/
-./sbin/start-master.sh --ip bogdan-Aspire-V5-571PG --port 7077 --webui-port 8080
-
-./bin/spark-class org.apache.spark.deploy.worker.Worker spark://bogdan-Aspire-V5-571PG:7077
