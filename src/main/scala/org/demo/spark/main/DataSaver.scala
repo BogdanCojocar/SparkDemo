6ab6060559f7b4d96328f5fcff983d04aa9e3e96
BogdanCojocar <bogdan.cojocar@gmail.com>
2014-12-20 18:55:39 +0200

    removed mongodbObj, used instead bsonObj

--- src/main/scala/org/demo/spark/main/DataSaver.scala
+++ src/main/scala/org/demo/spark/main/DataSaver.scala
@@ -6,11 +6,10 @@ import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.hadoop.conf.Configuration
 import org.bson.BasicBSONObject
-import com.mongodb.casbah.commons.MongoDBList
-import com.mongodb.casbah.commons.MongoDBObject
+import com.mongodb.BasicDBList
 
 object DataSaver {
-  val mongoUri = "mongodb://127.0.0.1:27017/gpxdata"
+  val mongoUri = "mongodb://127.0.0.1:27017/geojson.out"
   val hadoopDummyFile = "file:///dummy"
 }
 
@@ -26,15 +25,23 @@ class DataSaver {
     val sparkConf = new SparkConf()
 
     val geoJsonData = gpxData.map(data => {
-      val geoJson = MongoDBObject(
-        "loc" -> MongoDBObject(
-          "type" -> "MultiPoint",
-          "coordinates" -> MongoDBList(data)))
+      val geoJson = new BasicBSONObject()
+      var coordinates = new BasicDBList()
 
+      data.foreach(gpxData => {
+        val coordinate = new BasicBSONObject()
+        coordinate.put("lat", gpxData.lat)
+        coordinate.put("lon", gpxData.lon)
+        coordinates.add(coordinate)
+      })
+
+      val geoJsonData = new BasicBSONObject()
+      geoJsonData.put("type", "MultiPoint")
+      geoJsonData.put("coordinates", coordinates)
+      geoJson.put("loc", geoJsonData)
       (null, geoJson)
     })
 
     geoJsonData.saveAsNewAPIHadoopFile(DataSaver.hadoopDummyFile, classOf[Any], classOf[Any], classOf[com.mongodb.hadoop.MongoOutputFormat[Any, Any]], config)
   }
-
 }
--- src/main/scala/org/demo/spark/main/SparkDemo.scala
+++ src/main/scala/org/demo/spark/main/SparkDemo.scala
@@ -2,5 +2,11 @@ package org.demo.spark.main
 
 object SparkDemo {
   def main(args: Array[String]) {
+    
+    val dataHandler = new GPXDataHandler()
+    val data = dataHandler.parse()
+    
+    val dataSaver = new DataSaver()
+    dataSaver.save(data)
   }
 }
